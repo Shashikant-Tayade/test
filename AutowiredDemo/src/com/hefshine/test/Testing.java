18ef7fe9878c82930a606feec11a3d8682681ed0
Lenovo <Lenovo@Lenovo-PC>
2020-11-05 00:10:49 +0530

    commit

---
+++ AutowiredDemo/src/com/hefshine/test/Testing.java
@@ -0,0 +1,22 @@
+package com.hefshine.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hefshine.bean.Employee;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ApplicationContext con = new ClassPathXmlApplicationContext("config.xml");
+		
+		Employee emp = (Employee) con.getBean("e");
+		
+		emp.work();
+		
+	}
+
+}
